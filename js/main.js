e00760b1f8aada21b45167a0064239b62eaa074a
Dana D <61254757+danadd@users.noreply.github.com>
2021-04-09 15:55:12 -0500

    fixed 'My Story' page
    Fixed "my story" page where clicking/tapping the X in the navigation menu would not close the navigation bar. JS file has been updated to fix this.

--- js/main.js
+++ js/main.js
@@ -1,39 +1,40 @@
 const date = document.getElementById("date");
 date.innerHTML = new Date().getFullYear();
 
+const mainPage = document.getElementById("main");
+const mainStoryPage = document.getElementById("presi-main");
+const navSlideWidth =  document.getElementById("navislide");
+
 function openNav() {
     document.getElementById("navislide").style.width = "250px";
-
-
-    const navSlideWidth =  document.getElementById("navislide");
-    const navWidth = navSlideWidth.getBoundingClientRect().width;
-    const mainPage = document.getElementById("main");
     const pageWidth = mainPage.getBoundingClientRect().width;
 
     //logic for mobile
     if (pageWidth < 450){
       navSlideWidth.style.width = "200px";
       document.getElementById("main").style.marginLeft = "0px";
+      document.getElementById("presi-main").style.marginLeft = "0px";
     }
     else{
       navSlideWidth.style.width ="250px"
       document.getElementById("main").style.marginLeft = "250px";
+      document.getElementById("pres-main").style.marginLeft = "250px";
     }
   };
 
 
   function closeNav() {
-    const mainPage = document.getElementById("main");
-    const pageWidth = mainPage.getBoundingClientRect().width;
-    
     document.getElementById("navislide").style.width = "0";
+    const pageWidth = mainPage.getBoundingClientRect().width;
 
     //logic for mobile
     if (pageWidth < 450){
       mainPage.style.marginLeft = "0px";
+      mainStoryPage.style.marginLeft = "0px";
     } 
     else {
       mainPage.style.marginLeft = "200px";
+      mainStoryPage.style.marginLeft = "200px";
     }
   };
 
